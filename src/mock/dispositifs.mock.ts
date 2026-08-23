3709d2c25989c55485aec970de34a820bab294e1
abbv75 <bore.younous59@gmail.com>
2026-08-23 23:11:36 +0000

    feat: implement Dispositifs management page with list, header, and card components using mock data

---
+++ src/mock/dispositifs.mock.ts
@@ -0,0 +1,41 @@
+export const MOCK_DISPOSITIFS = [
+  {
+    id: 1,
+    libelle: "Activité Génératrice de Revenus",
+    code: "AGR",
+    version: "1.2",
+    montant_min: 100000,
+    montant_max: 500000,
+    taux: 0,
+    duree: 24,
+    cycles: 12,
+    dossiers: 142,
+    color: "#E7722B"
+  },
+  {
+    id: 2,
+    libelle: "Micro, Petites et Moyennes Entreprises",
+    code: "MPME",
+    version: "2.0",
+    montant_min: 1000000,
+    montant_max: 5000000,
+    taux: 2,
+    duree: 36,
+    cycles: 8,
+    dossiers: 87,
+    color: "#20A83A"
+  },
+  {
+    id: 3,
+    libelle: "Projets Structurants",
+    code: "PS",
+    version: "1.0",
+    montant_min: 5000000,
+    montant_max: 50000000,
+    taux: 5,
+    duree: 60,
+    cycles: 15,
+    dossiers: 12,
+    color: "#2D6BD4"
+  }
+]

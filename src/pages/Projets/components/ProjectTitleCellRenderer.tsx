74b0f7d4ef6a258060a76d0b6b7744cadf10bca3
Gloire Mani <crescencegracemani@gmail.com>
2026-09-07 21:30:24 +0100

    feat: fix build

--- src/pages/Projets/components/ProjectTitleCellRenderer.tsx
+++ src/pages/Projets/components/ProjectTitleCellRenderer.tsx
@@ -5,7 +5,7 @@ export const ProjectTitleCellRenderer = (params: ICellRendererParams<PROJET_T>) 
   if (!params.data) return null
   return (
     <div className="flex flex-col justify-center h-full">
-      <span className="font-semibold text-[#131C29] truncate">{params.data.titre}</span>
+      <span className="font-semibold text-[#131C29] truncate">{params.data.intitule}</span>
       <span className="text-[11px] text-slate-500 font-medium leading-none">{params.data.dispositif}</span>
     </div>
   )
